d4f249c512973ac086d75af6e598cc9c405d5789
Fraz Ikram <frazikram1@gmail.com>
2018-10-22 15:39:06 -0400

    Added navbar, worked on recipe list component

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import{Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+recipes: Recipe[]=[
+	new Recipe('A Test Recipe', 'This is simply a test',
+	'https://img.taste.com.au/UCkD8VfP/w1200-h630-cfill/taste/2016/11/chicken-and-prosciutto-parmigiana-79468-1.jpeg')
+];
   constructor() { }
 
   ngOnInit() {
